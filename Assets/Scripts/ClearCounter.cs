81c8bd0becf63ef63fc60db0b6fe9772c003e716
jlw_4049 <jessielw4049@gmail.com>
2024-09-14 10:53:51 -0400

    feat: initial commit

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearCounter : BaseCounter {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public override void Interact(Player player) {
+
+    }
+
+}
